0b1411d1d4588f63feee45ee79a9f2b11abe6fe4
Mattaveli <matthew0495@outlook.com>
2022-07-13 23:09:21 -0400

    Update logic

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-function updateInput() {
-  document.querySelector(".input").setAttribute("value", currentInput);
-}
-
 let doMath = {
   add(x, y = 0) {
     return x + y;
@@ -28,25 +24,43 @@ let secondNum;
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
+    console.log("clicked: " + button.value + " currentInput: " + currentInput);
+    console.log("firstNum:" + firstNum + " secondNum: " + secondNum);
     if (button.classList.contains("number")) {
       currentInput += button.value;
-      console.log("currentInput: " + currentInput);
       updateInput();
     }
     if (button.value == "clear") {
       currentInput = "";
+      firstNum = "";
+      secondNum = "";
       updateInput();
     }
     if (button.className == "operator" && button.value != "equals") {
-      currentOperator = button.value;
-      console.log(currentOperator);
-      firstNum = currentInput;
+      //currentOperator = button.value;
+      if (!firstNum) {
+        firstNum = currentInput;
+        currentInput = "";
+      }
+      if (!secondNum) {
+        secondNum = currentInput;
+        console.log("added 2nd num: " + secondNum);
+        currentInput = "";
+      }
+      if (firstNum && secondNum) {
+        let newValue = operate(firstNum, secondNum);
+        /* currentInput = newValue;
+        updateInput();
+        secondNum = "";
+        firstNum = newValue;*/
+        //currentOperator = "";
+      }
       currentInput = "";
-      updateInput();
+      //updateInput();
     }
     if (button.value == "equals") {
+      console.log("operator: " + currentOperator);
       if (!firstNum) {
-        console.log("notFirstNum");
         firstNum = currentInput;
         currentInput = "";
         return;
@@ -54,23 +68,73 @@ buttons.forEach((button) => {
       if (!currentInput) {
         return;
       }
-      secondNum = currentInput;
+      if (!secondNum) {
+        secondNum = currentInput;
+      }
 
-      console.log("firstNum:" + firstNum + " secondNum: " + secondNum);
-      let newValue = doMath[currentOperator](
-        parseInt(firstNum),
-        parseInt(secondNum)
-      );
-      currentInput = newValue;
+      if (currentOperator == "" || currentOperator == "equals") {
+        currentInput = firstNum;
+        updateInput();
+        return;
+      }
+      let newValue = operate(firstNum, secondNum);
+      /* currentInput = newValue;
       updateInput();
-      secondNum = 0;
       firstNum = newValue;
-      currentOperator = "";
+      currentInput = "";
+      secondNum = "";
+      currentOperator = "";*/
     }
 
     if (button.value == "inverse") {
-      currentInput = parseInt(currentInput) * -1;
+      currentInput = document.querySelector(".input").value;
+      currentInput = parseFloat(currentInput) * -1;
+      updateInput();
+    }
+
+    if (button.value == "percent") {
+      currentInput = document.querySelector(".input").value;
+      currentInput = parseFloat(currentInput) * 0.01;
+      updateInput();
+    }
+
+    if (button.value == ".") {
+      currentInput = document.querySelector(".input").value;
+      console.log("in decimal" + currentInput);
+      if (currentInput.includes(".")) return;
+      addDecimal();
       updateInput();
     }
   });
 });
+
+window.addEventListener("keydown", userKeyPress);
+
+function updateInput() {
+  document.querySelector(".input").setAttribute("value", currentInput);
+}
+
+function userKeyPress(e) {
+  console.log(e);
+  if (e.key == "Backspace") deleteNumber();
+}
+
+function deleteNumber() {
+  currentInput = currentInput.slice(0, -1);
+  updateInput();
+}
+
+function addDecimal() {
+  currentInput += ".";
+}
+
+function operate(num1, num2) {
+  let newValue = doMath[currentOperator](parseFloat(num1), parseFloat(num2));
+  currentInput = newValue;
+  updateInput();
+  firstNum = newValue;
+  currentInput = "";
+  secondNum = "";
+  //currentOperator = "";
+  return newValue;
+}
